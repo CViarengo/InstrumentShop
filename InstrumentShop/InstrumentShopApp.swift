34043148a6c9bfa8133e7b7d1a1985bb18f40c1a
CViarengo <100734875+CViarengo@users.noreply.github.com>
2022-12-27 21:42:27 -0500

    Initial Commit

---
+++ InstrumentShop/InstrumentShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentShopApp.swift
+//  InstrumentShop
+//
+//  Created by Chris Viarengo on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
